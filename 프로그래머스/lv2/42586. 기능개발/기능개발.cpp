80735383f062e43813c41e27f673d9f57654d402
Dahye Kim <57945707+ekgp3987@users.noreply.github.com>
2023-06-03 15:52:35 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 3.77 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> answer;
+ 
+    while(progresses.size())
+    {
+        for(int j=0; j<progresses.size(); ++j)
+        { 
+            progresses[j] += speeds[j];
+        }
+       
+        if(progresses.front() >= 100)
+        {
+            int cnt =0;
+            while(!(progresses.front() < 100))
+            {
+                progresses.erase(progresses.begin());
+                speeds.erase(speeds.begin());
+                cnt++;
+                
+                if (!progresses.size()) break;
+            }
+            answer.push_back(cnt);
+        }
+    }    
+    
+    return answer;
+}
