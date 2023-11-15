62f6686a5beaf2c4115377638177713bac91c635
Dahye Kim <57945707+ekgp3987@users.noreply.github.com>
2023-11-15 17:23:58 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+int d[1000001];
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	d[1] = 1; 
+	d[2] = 2;
+	d[3] = 4;
+	for (int i = 4; i < 11; ++i)
+	{
+		d[i] = d[i - 1] + d[i-2] + d[i-3];
+	}
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n; 
+		cin >> n;
+		cout << d[n] << '\n';
+	}
+
+	return 0;
+}
