bd64293abef683ab07f73af729b392e5d81f041a
Dahye Kim <57945707+ekgp3987@users.noreply.github.com>
2023-11-20 15:15:52 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.24 ms, Memory: 3.99 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -1,53 +1,44 @@
 #include <bits/stdc++.h>
 
+
+int dx[4] = {1,-1,0,0};
+int dy[4] = {0,0,1,-1};
+
+int visited[102][102];
+
 using namespace std;
-
-// 방향 정의
-int dx[] = {-1, 1, 0, 0};
-int dy[] = {0, 0, -1, 1};
-
 int solution(vector<vector<int> > maps)
 {
-    int n = maps.size();    
-    int m = maps[0].size(); 
     
-    int visit[101][101] ={};
+    int n= maps.size();
+    int m= maps[0].size();
     
-    queue<pair<int,int>> que;
-    que.push({0,0}); // 시작점
+    queue<pair<int,int>> q;
+    q.push({0,0});
     
-    
-    while(!que.empty()) // 큐 빌때까지 반복
+    while(!q.empty())
     {
-        int x = que.front().first;
-        int y = que.front().second;
+        auto cur = q.front(); q.pop();
         
-        if(x == n-1 && y == m-1) return maps[x][y];
+        // goal
+        if(cur.first == n-1 && cur.second == m-1) return maps[cur.first][cur.second];
         
-        que.pop();
-        
-        for(int i =0; i<4;i++)
+        // 4 dir
+        for(int dir=0; dir<4; ++dir)
         {
-            int NextX = x+dx[i];
-            int NextY = y+dy[i];
+            int nx = cur.first + dx[dir];
+            int ny = cur.second + dy[dir];
             
-            // 게임 맵을 벗어나는 경우 무시
-            if(NextX < 0 || NextX >= n|| NextY < 0 || NextY >= m) 
-                continue;
-            // 벽인 경우 무시, 방문한 경우 무시
-            if(maps[NextX][NextY] == 0 || visit[NextX][NextY] )
-                continue;
+            // 맵 외부, 방문여부, 벽 판별
+            if(nx <0 || nx>=n || ny <0 || ny>=m) continue;
+            if(visited[nx][ny] || !maps[nx][ny]) continue;
             
-            // 길
-            if(maps[NextX][NextY] == 1)
-            {
-                maps[NextX][NextY] = maps[x][y] + 1;
-                que.push({NextX, NextY});
-                visit[NextX][NextY] = 1;
-                
-            }
+            // update 
+            maps[nx][ny] = maps[cur.first][cur.second] +1;
+            visited[nx][ny] =1;
+            q.push({nx,ny});
         }
-        
     }
+
     return -1;
 }
