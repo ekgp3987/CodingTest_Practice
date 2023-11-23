b7a8ae2d2ef27dfb1ba5597674e0d24ce96fdf21
Dahye Kim <57945707+ekgp3987@users.noreply.github.com>
2023-11-23 18:02:42 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -10,13 +10,8 @@ int main()
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
-	int t, n;
-	cin >> t;
-
-	cnt0[0] = 1;
-	cnt1[0] = 0;
-	cnt0[1] = 0;
-	cnt1[1] = 1;
+	cnt0[0] = 1; cnt1[0] = 0;
+	cnt0[1] = 0; cnt1[1] = 1;
 
 	for (int i = 2; i < 41; ++i)
 	{
@@ -24,15 +19,14 @@ int main()
 		cnt1[i] = cnt1[i - 1] + cnt1[i - 2];
 	}
 
+	int t, n;
+	cin >> t;
 
 	while (t--)
 	{
 		cin >> n;
-
 		cout << cnt0[n] << ' ' << cnt1[n] << '\n';
-
 	}
-
 
 	return 0;
 }
