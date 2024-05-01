d6f0bc95c3251cd2beaabff9c36e012895adddf5
Dahye Kim <57945707+ekgp3987@users.noreply.github.com>
2024-05-01 15:03:20 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.35 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -1,44 +1,42 @@
 #include <bits/stdc++.h>
 
-
-int dx[4] = {1,-1,0,0};
-int dy[4] = {0,0,1,-1};
+int dx[4] = {0,0,1,-1};
+int dy[4] = {1,-1,0,0};
 
 int visited[102][102];
 
 using namespace std;
 int solution(vector<vector<int> > maps)
 {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     
-    int n= maps.size();
-    int m= maps[0].size();
+    int n = maps.size();
+    int m = maps[0].size();
     
     queue<pair<int,int>> q;
-    q.push({0,0});
-    
+    q.push({0,0}); // start point
+   
     while(!q.empty())
     {
-        auto cur = q.front(); q.pop();
+       auto cur = q.front(); q.pop();
         
-        // goal
-        if(cur.first == n-1 && cur.second == m-1) return maps[cur.first][cur.second];
+        if(cur.first == n-1 && cur.second == m-1) 
+            return maps[cur.first][cur.second];
         
-        // 4 dir
         for(int dir=0; dir<4; ++dir)
         {
             int nx = cur.first + dx[dir];
-            int ny = cur.second + dy[dir];
+            int ny = cur.second+ dy[dir];
             
-            // 맵 외부, 방문여부, 벽 판별
-            if(nx <0 || nx>=n || ny <0 || ny>=m) continue;
+            if(nx <0 || nx > n-1 || ny <0 || ny >m-1) continue;
             if(visited[nx][ny] || !maps[nx][ny]) continue;
             
-            // update 
             maps[nx][ny] = maps[cur.first][cur.second] +1;
-            visited[nx][ny] =1;
+            visited[nx][ny]  = 1;
             q.push({nx,ny});
-        }
+        } 
     }
-
+    
     return -1;
 }
